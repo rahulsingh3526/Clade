d520ed36783ba28ba153d7db75acfa6cc3edfdca
Rahul_Singh <koderrahul@gmail.com>
2024-06-28 17:32:18 +0530

    feat: sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { JobPreview } from "@/components/job-preview";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Tabs defaultValue="jobs" className="w-full my-4 ml-24">
+      <Tabs defaultValue="jobs" className="w-full py-4 pl-16 ">
         <TabsList className="">
           <TabsTrigger value="jobs">Job preview</TabsTrigger>
           <TabsTrigger value="applicants">Applicants</TabsTrigger>
           <TabsTrigger value="match">Match</TabsTrigger>
           <TabsTrigger value="messages">Messages</TabsTrigger>
         </TabsList>
+        <Separator className="h-[2px]  flex self-auto" />
         <TabsContent value="jobs">
           <JobPreview />
         </TabsContent>
